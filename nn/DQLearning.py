ade4b9d6bcbae19191d771a3f2e2d3075a78f066
d.preti <d.preti@almawave.it>
2021-03-12 10:06:31 +0100

    updated env description

--- nn/DQLearning.py
+++ nn/DQLearning.py
@@ -93,6 +93,23 @@ if __name__=="__main__":
         Num   Action
         0     Push cart to the left
         1     Push cart to the right
+
+    Reward:
+        Reward is 1 for every step taken, including the termination step
+
+    Starting State:
+        All observations are assigned a uniform random value in [-0.05..0.05]
+
+    Episode Termination:
+        Pole Angle is more than 12 degrees.
+        Cart Position is more than 2.4 (center of the cart reaches the edge of
+        the display).
+        Episode length is greater than 200.
+        Solved Requirements:
+        Considered solved when the average return is greater than or equal to
+        195.0 over 100 consecutive trials.
+    """
+
     """
     optimizer = tf.keras.optimizers.Adam(learning_rate=0.01)
     agent = DeepQAgent(enviroment=env, optimizer=optimizer)
