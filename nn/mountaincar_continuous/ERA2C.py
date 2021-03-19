d103a869cf2268c41275bf92f9db33c36db6582d
d.preti <d.preti@almawave.it>
2021-03-19 17:52:12 +0100

    A2C and ExperienceReplay A2C implemented for mountaincar continuous env

--- nn/mountaincar_continuous/ERA2C.py
+++ nn/mountaincar_continuous/ERA2C.py
@@ -30,7 +30,9 @@ class Actor:
         state = np.reshape(state, [1, self.state_dim])
         mu, std = self.model.predict(state)
         mu, std = mu[0], std[0]
-        return np.random.normal(mu, std, size=self.action_dim)
+        a = np.random.normal(mu, std, size=self.action_dim)
+        return  np.clip(a, -self.action_bound, self.action_bound)
+
 
     def log_pdf(self, mu, std, action):
         std = tf.clip_by_value(std, self.std_bound[0], self.std_bound[1])
@@ -41,7 +43,7 @@ class Actor:
 
     def compute_loss(self, mu, std, actions, advantages):
         log_policy_pdf = self.log_pdf(mu, std, actions)
-        loss_policy = log_policy_pdf * advantages
+        loss_policy = log_policy_pdf * (advantages)
         return tf.reduce_sum(-loss_policy)
         
     def train(self, states, actions, advantages):
@@ -55,8 +57,8 @@ class Actor:
 class Critic:
     def __init__(self, state_dim):
         self.state_dim = state_dim
-        self.model = self.create_model()
         self.opt = tf.keras.optimizers.Adam(learning_rate=0.001)
+        self.model = self.create_model()
 
     def create_model(self):
         state_input = tf.keras.layers.Input((self.state_dim,))
@@ -64,22 +66,10 @@ class Critic:
         dense_2 = tf.keras.layers.Dense(32, activation='relu')(dense_1)
         dense_3 = tf.keras.layers.Dense(16, activation='relu')(dense_2)
         v       = tf.keras.layers.Dense(1, activation='linear')(dense_3)
-        return tf.keras.models.Model(state_input, v)
-
-    def compute_loss(self, v_pred, td_targets):
-        mse = tf.keras.losses.MeanSquaredError()
-        return mse(td_targets, v_pred)
-
-    def train(self, states, td_targets):
-        with tf.GradientTape() as tape:
-            v_pred = self.model(states, training=True)
-            assert v_pred.shape == td_targets.shape
-            loss = self.compute_loss(v_pred, tf.stop_gradient(td_targets))
-        grads = tape.gradient(loss, self.model.trainable_variables)
-        self.opt.apply_gradients(zip(grads, self.model.trainable_variables))
-        return loss
-
-
+        model   = tf.keras.models.Model(state_input, v)
+        model.compile(loss='mse', optimizer=self.opt)
+        return model 
+        
 class A2CAgent:
     def __init__(self, env):
         self.env = env
@@ -88,26 +78,27 @@ class A2CAgent:
         self.action_bound = self.env.action_space.high[0]
         self.std_bound = [1e-3, 1.0]
         self.gamma = 0.9
+        self.experience_replay = deque(maxlen=400000)
+
         self.actor = Actor(self.state_dim, self.action_dim,
                            self.action_bound, self.std_bound)
         self.critic = Critic(self.state_dim)
 
-    def td_target(self, reward, next_state, done):
-        if done:
-            return reward
-        v_value = self.critic.model.predict(
-            np.reshape(next_state, [1, self.state_dim]))
-        return np.reshape(reward + self.gamma * v_value[0], [1, 1])
+    def store(self, state, action, reward, next_state, terminated):
+        self.experience_replay.append((state, action, reward, next_state, terminated))
 
-    def advantage(self, td_targets, baselines):
-        return td_targets - baselines
-
-    def list_to_batch(self, list):
-        batch = list[0]
-        for elem in list[1:]:
-            batch = np.append(batch, elem, axis=0)
-        return batch
-
+    def parallel_train(self,batch_size):
+        minibatch = random.sample(self.experience_replay, batch_size)
+        td_targets = np.zeros(batch_size)
+        states = np.vstack([x[0] for x in minibatch])
+        actions = np.array([x[1] for x in minibatch])
+        rewards = np.array([x[2] for x in minibatch])
+        next_states = np.vstack([x[3] for x in minibatch])
+        episodes_done = np.array([not x[4] for x in minibatch])
+        td_targets[range(batch_size)] = rewards + self.gamma * self.critic.model.predict(next_states)[0] * episodes_done
+        advantages = td_targets - self.critic.model.predict(states)
+        actor_loss = self.actor.train(states, actions, advantages)
+        critic_loss = self.critic.model.fit(states, td_targets, epochs=1, verbose=0, batch_size=batch_size)
             
 if __name__=="__main__":
 
@@ -118,60 +109,41 @@ if __name__=="__main__":
 
     agent.actor.model.summary()
     agent.critic.model.summary()
-    batch_size=128
-    ep = 0 
+    batch_size=64
+
     won = 0
     num_of_episodes = 1000
     for i_episode in tqdm(range(num_of_episodes)):
-        state_batch = []
-        action_batch = []
-        td_target_batch = []
-        advantage_batch = []
+        
         episode_reward, done = 0, False
-
         state = env.reset()
-       
+        state = tf.expand_dims(state, axis=0)
         while True:
             #env.render()
             action = agent.actor.get_action(state)
-            action = np.clip(action, -agent.action_bound, agent.action_bound)
-
             next_state, reward, done, _ = env.step(action)
 
-            state = np.reshape(state, [1, agent.state_dim])
-            action = np.reshape(action, [1, agent.action_dim])
-            next_state = np.reshape(next_state, [1, agent.state_dim])
-            reward = np.reshape(reward, [1, 1])
+            action = tf.expand_dims(action, axis=0)
+            next_state = tf.expand_dims(next_state, axis=0)
+            agent.store(state, action, reward, next_state, done)
 
-            td_target = agent.td_target(reward, next_state, done)
-            advantage = agent.advantage(
-                td_target, agent.critic.model.predict(state))
+            if len(agent.experience_replay) > batch_size:    
+                agent.parallel_train(batch_size)
 
-            state_batch.append(state)
-            action_batch.append(action)
-            td_target_batch.append(td_target)
-            advantage_batch.append(advantage)
-
-            if len(state_batch) >= batch_size or done:     #this is ugly...bettere experience replay
-                states = agent.list_to_batch(state_batch)
-                actions = agent.list_to_batch(action_batch)
-                td_targets = agent.list_to_batch(td_target_batch)
-                advantages = agent.list_to_batch(advantage_batch)
-                actor_loss = agent.actor.train(states, actions, advantages)
-                critic_loss = agent.critic.train(states, td_targets)
-
-                state_batch = []
-                action_batch = []
-                td_target_batch = []
-                advantage_batch = []
-
-            episode_reward += reward[0][0]
-            state = next_state[0]
-        
+            episode_reward += reward
+            state = next_state
+            
             if done:
-              if state[0] >= 0.45:
+              if np.mean(state[0][0]) >= 0.45:
                 won+=1
                 print('won')
               if (i_episode+1)%1==0:
                 print("Episode {} finished with {} mean reward".format(i_episode+1,episode_reward))
               break
+
+        if won==100:
+            print('--- won ---')
+            agent.q_network.save('./models/a2c.hdf5',overwrite=True,include_optimizer=False)
+            break
+          
+    
