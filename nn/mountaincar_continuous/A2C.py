3aa8a3699a62a6962e5e980f8e809466f571b952
David <preti.david@gmail.com>
2021-03-22 23:39:46 +0100

    a2c with tfp and entropy

--- nn/mountaincar_continuous/A2C.py
+++ nn/mountaincar_continuous/A2C.py
@@ -6,7 +6,7 @@ import progressbar
 import matplotlib.pyplot as plt 
 import random
 from tqdm import tqdm
-
+import tensorflow_probability as tfp
 
 class Actor:
     def __init__(self, state_dim, action_dim, action_bound, std_bound):
@@ -18,12 +18,16 @@ class Actor:
         self.opt = tf.keras.optimizers.Adam(learning_rate=0.001)
 
     def create_model(self):
+        
+        def rescale(a):
+            return a*tf.constant(self.action_bound)
+
         state_input = tf.keras.layers.Input((self.state_dim,))
-        dense_1 = tf.keras.layers.Dense(32, activation='relu')(state_input)
+        dense_1 = tf.keras.layers.Dense(50, activation='relu')(state_input)
         dense_2 = tf.keras.layers.Dense(32, activation='relu')(dense_1)
         out_mu = tf.keras.layers.Dense(self.action_dim, activation='tanh')(dense_2)
-        mu_output = tf.keras.layers.Lambda(lambda x: x * self.action_bound)(out_mu)
-        std_output = tf.keras.layers.Dense(self.action_dim, activation='softplus')(dense_2) #why softplus instead of exp???
+        mu_output = tf.keras.layers.Lambda(lambda x: rescale(x))(out_mu)
+        std_output = tf.keras.layers.Dense(self.action_dim, activation='softplus')(dense_2) 
         return tf.keras.models.Model(state_input, [mu_output, std_output])
 
     def get_action(self, state):
@@ -32,17 +36,19 @@ class Actor:
         mu, std = mu[0], std[0]
         return np.random.normal(mu, std, size=self.action_dim)
 
-    def log_pdf(self, mu, std, action):
-        std = tf.clip_by_value(std, self.std_bound[0], self.std_bound[1])
-        var = std ** 2
-        log_policy_pdf = -0.5 * (action - mu) ** 2 / \
-            var - 0.5 * tf.math.log(var * 2 * np.pi)
-        return tf.reduce_sum(log_policy_pdf, 1, keepdims=True)
+    # def log_pdf(self, mu, std, action):
+    #     std = tf.clip_by_value(std, self.std_bound[0], self.std_bound[1])
+    #     var = std ** 2
+    #     log_policy_pdf = -0.5 * (action - mu) ** 2 / \
+    #         var - 0.5 * tf.math.log(var * 2 * np.pi)
+    #     return tf.reduce_sum(log_policy_pdf, 1, keepdims=True)
 
     def compute_loss(self, mu, std, actions, advantages):
-        log_policy_pdf = self.log_pdf(mu, std, actions)
-        loss_policy = log_policy_pdf * advantages
-        return tf.reduce_sum(-loss_policy)
+        #log_policy_pdf = self.log_pdf(mu, std, actions)
+        #log_policy_pdf = dist.log_prob(value=actions)
+        dist = tfp.distributions.Normal(loc=mu, scale=std)
+        loss_policy = (-dist.log_prob(value=actions) * advantages + 0.001*dist.entropy())
+        return tf.reduce_sum(loss_policy)
         
     def train(self, states, actions, advantages):
         with tf.GradientTape() as tape:
@@ -60,7 +66,7 @@ class Critic:
 
     def create_model(self):
         state_input = tf.keras.layers.Input((self.state_dim,))
-        dense_1 = tf.keras.layers.Dense(32, activation='relu')(state_input)
+        dense_1 = tf.keras.layers.Dense(50, activation='relu')(state_input)
         dense_2 = tf.keras.layers.Dense(32, activation='relu')(dense_1)
         dense_3 = tf.keras.layers.Dense(16, activation='relu')(dense_2)
         v       = tf.keras.layers.Dense(1, activation='linear')(dense_3)
@@ -70,6 +76,7 @@ class Critic:
         mse = tf.keras.losses.MeanSquaredError()
         return mse(td_targets, v_pred)
 
+    @tf.function
     def train(self, states, td_targets):
         with tf.GradientTape() as tape:
             v_pred = self.model(states, training=True)
@@ -87,7 +94,7 @@ class A2CAgent:
         self.action_dim = self.env.action_space.shape[0]
         self.action_bound = self.env.action_space.high[0]
         self.std_bound = [1e-3, 1.0]
-        self.gamma = 0.9
+        self.gamma = 0.99
         self.actor = Actor(self.state_dim, self.action_dim,
                            self.action_bound, self.std_bound)
         self.critic = Critic(self.state_dim)
@@ -118,7 +125,7 @@ if __name__=="__main__":
 
     agent.actor.model.summary()
     agent.critic.model.summary()
-    batch_size=128
+    batch_size=8
     ep = 0 
     won = 0
     num_of_episodes = 1000
@@ -137,7 +144,9 @@ if __name__=="__main__":
             action = np.clip(action, -agent.action_bound, agent.action_bound)
 
             next_state, reward, done, _ = env.step(action)
-
+            if done and next_state[0] >= 0.45:
+                reward += 500 
+            reward += (next_state[0]-0.5)
             state = np.reshape(state, [1, agent.state_dim])
             action = np.reshape(action, [1, agent.action_dim])
             next_state = np.reshape(next_state, [1, agent.state_dim])
@@ -152,7 +161,7 @@ if __name__=="__main__":
             td_target_batch.append(td_target)
             advantage_batch.append(advantage)
 
-            if len(state_batch) >= batch_size or done:     #this is ugly...bettere experience replay
+            if len(state_batch) >= batch_size or done:    
                 states = agent.list_to_batch(state_batch)
                 actions = agent.list_to_batch(action_batch)
                 td_targets = agent.list_to_batch(td_target_batch)
@@ -167,7 +176,7 @@ if __name__=="__main__":
 
             episode_reward += reward[0][0]
             state = next_state[0]
-        
+
             if done:
               if state[0] >= 0.45:
                 won+=1
@@ -176,8 +185,9 @@ if __name__=="__main__":
                 print("Episode {} finished with {} mean reward".format(i_episode+1,episode_reward))
               break
 
-        if won==100:
+        if won==50:
             print('--- won ---')
-            agent.actor.model.save('./models/a2c.hdf5',overwrite=True,include_optimizer=False)
+            agent.actor.model.save('./models/actor.hdf5',overwrite=True,include_optimizer=False)
+            agent.critic.model.save('./models/critic.hdf5',overwrite=True,include_optimizer=False)
             break
           
